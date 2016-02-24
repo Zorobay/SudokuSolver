8e56ab241ecdcd557181b2e10e226f012c9bd65f
Sebastian Hegardt <se.hegardt@gmail.com>
2016-02-24 09:43:48 +0100

    Started on solve method and helper method (legalNumber)

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,4 +1,27 @@
 
 public class SudokuSolver {
 
+	private int[][] sudokuBoard;
+	private static final int boardSize = 9;
+	
+	public SudokuSolver(){
+		sudokuBoard = new int[boardSize][boardSize];
+	}
+	
+	public boolean solve(){
+		return solve(0,0);
+	}
+	
+	private boolean solve(int row, int col){
+		if(sudokuBoard[row][col] == 0){ //rutan är ej ifylld
+			for(int i = 1; i <= 9;)
+		}
+	}
+	
+	private boolean legalNumber(int num, int row, int col){
+		boolean legal = true;
+		for(int i = 0; i < boardSize -1; i++){ //check row
+			
+		}
+	}
 }
