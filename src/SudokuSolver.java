0affda347e35a46ad8542e0275d2911615c62d2b
Sebastian Hegardt <se.hegardt@gmail.com>
2016-03-03 23:22:33 +0100

    Completed solve method
    Completed solve and legalNumber methods. The Sudoku solver is now ready
    to be used and has been sucessfully tested on a 9x9 sudoku

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,27 +1,98 @@
 
 public class SudokuSolver {
 
-	private int[][] sudokuBoard;
+	public int[][] sudokuBoard;
 	private static final int boardSize = 9;
 	
 	public SudokuSolver(){
 		sudokuBoard = new int[boardSize][boardSize];
 	}
 	
+	public boolean put(int num, int row, int col){
+		row = row - 1;
+		col = col - 1;
+		if(num < 1 || num > 9 || row < 0 || row > 8 || col < 0 || col > 8){
+			return false;
+		}
+		
+		sudokuBoard[row][col] = num;
+		return true;
+	}
 	public boolean solve(){
 		return solve(0,0);
 	}
 	
 	private boolean solve(int row, int col){
+		if(col > boardSize -1){
+			col = 0;
+			row++;
+		}
+		if(row > boardSize - 1){
+			return true;
+		}
+		
 		if(sudokuBoard[row][col] == 0){ //rutan är ej ifylld
-			for(int i = 1; i <= 9;)
+			boolean canSolve = true;
+			for(int i = 1; i <= 9; i++){
+				if(legalNumber(i, row, col)){
+					sudokuBoard[row][col] = i;
+					canSolve = solve(row, col+1);
+				}
+			}
+			if(sudokuBoard[row][col] == 0 || !canSolve){
+				sudokuBoard[row][col] = 0;
+				return false;
+			}
+			return true;	
+		}
+		else{
+			if(!legalNumber(sudokuBoard[row][col], row, col)){
+				return false;
+			}
+			return solve(row, col+1);
 		}
 	}
 	
-	private boolean legalNumber(int num, int row, int col){
-		boolean legal = true;
-		for(int i = 0; i < boardSize -1; i++){ //check row
-			
+	public boolean legalNumber(int num, int row, int col){
+		for(int c = 0; c < boardSize; c++){ //check row
+			if(sudokuBoard[row][c] == num && c != col){ //already exists, return false;
+				return false;
+			}
 		}
+		for(int r = 0; r < boardSize; r++){
+			if(sudokuBoard[r][col] == num && r != row){
+				return false;
+			}
+		}
+		int startRow = row; int startCol = col;
+		while(startRow % 3 != 0){
+			startRow--;
+		}
+		while(startCol % 3 != 0){
+			startCol--;
+		}
+		
+		for (int c = startCol; c < startCol + 3; c++) {
+			for (int r = startRow; r < startRow + 3; r++) {
+				if(sudokuBoard[r][c] == num && c != col && r != row){
+					return false;
+				}
+			}
+
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		for(int[] row : sudokuBoard){
+			for(int num : row){
+				sb.append(num + " ");
+			}
+			sb.append("\n");
+		}
+		
+		return sb.toString();
 	}
 }
--- src/TestMain.java
+++ src/TestMain.java
@@ -2,8 +2,46 @@
 public class TestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SudokuSolver ss = new SudokuSolver();
+	//	System.out.println(ss.toString());
+		//System.out.println(ss.solve());
+		
+		ss.put(2, 1, 2);
+		ss.put(1, 1, 4);
+		ss.put(7, 1, 5);
+		ss.put(8, 1, 6);
+		ss.put(3, 1, 8);
+		ss.put(4, 2, 2);
+		ss.put(3, 2, 4);
+		ss.put(2, 2, 6);
+		ss.put(9, 2, 8);
+		ss.put(1, 3, 1);
+		ss.put(6, 3, 9);
+		ss.put(8, 4, 3);
+		ss.put(6, 4, 4);
+		ss.put(3, 4, 6);
+		ss.put(5, 4, 7);
+		ss.put(3, 5, 1);
+		ss.put(4, 5, 9);
+		ss.put(6, 6, 3);
+		ss.put(7, 6, 4);
+		ss.put(9, 6, 6);
+		ss.put(2, 6, 7);
+		ss.put(9, 7, 1);
+		ss.put(2, 7, 9);
+		ss.put(8, 8, 2);
+		ss.put(9, 8, 4);
+		ss.put(1, 8, 6);
+		ss.put(6, 8, 8);
+		ss.put(1, 9, 2);
+		ss.put(4, 9, 4);
+		ss.put(3, 9, 5);
+		ss.put(6, 9, 6);
+		ss.put(5, 9, 8);
+		
+		System.out.println(ss.toString());
+		System.out.println(ss.solve());
+		System.out.println(ss.toString());
 	}
-
+	
 }
